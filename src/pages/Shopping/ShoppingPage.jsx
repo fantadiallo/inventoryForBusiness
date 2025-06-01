1e719eedbb18271207f00c66df52d8a6d28b4a7f
fantadiallo <fatmbo0003@outlook.com>
2025-06-01 13:52:39 +0200

    Add Shopping pages under src/pages/Shopping

---
+++ src/pages/Shopping/ShoppingPage.jsx
@@ -0,0 +1,9 @@
+import ShoppingList from "../../components/Shopping/ShoppingList";
+
+export default function ShoppingPage() {
+  return (
+    <div className="container mt-4">
+      <ShoppingList />
+    </div>
+  );
+}
