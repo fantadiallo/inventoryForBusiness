acb7855cc48cb02388acc1ff91cde7dd7824fd5b
fantadiallo <fatmbo0003@outlook.com>
2025-06-01 13:52:21 +0200

    Add Products pages

---
+++ src/pages/Products/Products.jsx
@@ -0,0 +1,10 @@
+import AddPredefinedOrder from '../../components/AddPredefinedOrder/AddPredefinedOrder';
+
+export default function AddProductPage() {
+  return (
+    <div className="container mt-4">
+      <h1>Add Dish / Product Template</h1>
+      <AddPredefinedOrder />
+    </div>
+  );
+}
